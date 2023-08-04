d343ad6969822681ef3594eaacb7f8b3f1f73fc5
DrunKleen <reza.df.x@gmail.com>
2023-08-04 04:09:26 +0200

    camera movement added

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -57,19 +57,19 @@ class TileMap:
             if tile["type"] in PHYSICS_TILES
         ]
 
-    def render(
-        self,
-        surf,
-    ):
+    def render(self, surf, offset=(0, 0)):
         for tile in self.offgrid_tiles:
             surf.blit(
                 self.game.assets[tile["type"]][tile["variant"]],
-                tile["pos"],
+                (tile["pos"][0] - offset[0], tile["pos"][1] - offset[1]),
             )
 
         for location in self.tilemap:
             tile = self.tilemap[location]
             surf.blit(
                 self.game.assets[tile["type"]][tile["variant"]],
-                (tile["pos"][0] * self.tile_size, tile["pos"][1] * self.tile_size),
+                (
+                    tile["pos"][0] * self.tile_size - offset[0],
+                    tile["pos"][1] * self.tile_size - offset[1],
+                ),
             )

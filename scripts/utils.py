fbf2ecd1f743b6617ea49dd6f11ec934235f4e26
DrunKleen <reza.df.x@gmail.com>
2023-08-05 05:18:05 +0200

    good night save

--- scripts/utils.py
+++ scripts/utils.py
@@ -24,3 +24,12 @@ def load_images(path) -> list:
         load_image(f"{path}/{img_name}")
         for img_name in sorted(os.listdir(BASE_IMG_PATH + path))
     ]
+
+
+class Animation:
+    def __init__(self, images, image_duration=5, loop=True):
+        self.images = images
+        self.image_duration = image_duration
+        self.loop = loop
+        self.done = False
+        self.frame = 0
